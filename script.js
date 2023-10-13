4cd92c043dbe749440325c0b1db3eb115f00999f
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-10-13 14:14:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound(sound) {
+    console.log(sound);
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    this.makeSound("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    this.makeSound("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
